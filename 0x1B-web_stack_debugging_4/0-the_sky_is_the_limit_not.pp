daa0f6a8f973dc7d9f53ca89b15d1ada8df77579
Tsakani Mohale <tsakanimohale4@gmail.com>
2024-04-18 18:00:20 -0700

    Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# increace the request limit of Nginx
+exec { 'Change Nginx limit':
+  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+  provider => shell,
+}
